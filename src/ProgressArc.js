0e0224338a9a06089b41d4859aa4ee763e21e120
Webmaster444 <jlmobile710@gmail.com>
2017-11-06 22:18:54 +0100

    initial project

---
+++ src/ProgressArc.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import * as d3 from "d3";
+
+class ProgressArc extends Component {
+  render() {
+    return (
+      <div ref="arc"></div>
+    )
+  }
+}
+
+export default ProgressArc;
