9d3f5a0a8257a335f151a68518ac44757be39401
Webmaster444 <jlmobile710@gmail.com>
2017-11-13 16:06:37 +0100

    code change for state

--- src/WorkflowChart.js
+++ src/WorkflowChart.js
@@ -1,44 +1,478 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import * as d3 from "d3";
 
 class WorkflowChart extends Component {
 
-	displayName: 'WorkflowChart'; 
+    displayName: 'WorkflowChart';
 
-  propTypes: {
-    id: PropTypes.string,
-    height: PropTypes.number,
-    width: PropTypes.number,
-    innerRadius: PropTypes.number,
-    outerRadius: PropTypes.number,
-    backgroundColor: PropTypes.string,
-    foregroundColor: PropTypes.string,
-    percentComplete: PropTypes.number
-  }
+    propTypes: {
+        id: PropTypes.string,
+        height: PropTypes.number,
+        width: PropTypes.number,
+        innerRadius: PropTypes.number,
+        outerRadius: PropTypes.number,
+        backgroundColor: PropTypes.string,
+        foregroundColor: PropTypes.string,
+        percentComplete: PropTypes.number
+    }
 
-  componentDidMount() {
-    var generalChartData = require('./data/jsondata.json');    
-	  this.drawArc();
-	}
+    constructor(props) {
+      super(props);
+      this.state = {
+        svg: '',
+        width: 940,
+        height: 560,
+        defColor:'#337ab7',
+        priColor:'#f0ad4e',
+        totalItemsCnt:0,
+        depth: 0,
+        defElWidth : 100,
+        defElHeight: 40,
+        linkWidth: 50,
+        currentStep : 1,
+        padding: 5,
+        nextX: 0,
+        nextY: 40,
+        itemsData : [],
+        drawedItemsArray: [],
+        defStreamHeight:[],
+        paddingY: 40,
+        junctionOperatorRadius : 20,
+        rhombusRadius: 50
+      }       
+    }
 
-	componentDidUpdate() {
-    this.redrawArc();
-  }
+    componentDidMount() {
+      var generalChartData = require('./data/jsondata.json');
+      this.setState({svg : d3.select("#content").append("svg")
+          .attr('width', '100%')
+          .attr('height', '100%')
+          .attr('viewBox', '0 0 970 570')
+          .attr('preserveAspectRatio', 'xMinYMin')
+          .append("g")
+          .attr("transform", "translate(30,10)")});
+    }
 
-  redrawArc() {
-    const context = d3.select(`#${this.props.id}`);
-    context.remove();
-    this.drawArc();
-  }
+    componentDidUpdate() {
+        this.redrawArc();
+    }
 
-  drawArc() {   
-  }
-  
-  render() {
-    return (
-      <div ref="workflow"></div>
-    )
-  }
+    redrawArc() {
+        const context = d3.select(`#${this.props.id}`);
+        context.remove();
+        this.drawArc();
+    }
+
+    drawArc() {}
+
+    drawRect(id, x, y, width, height, color, text) {
+        var g = this.state.svg.append('g').attr('id', 'item' + id).attr('class', 'g_wrapper').attr('transform', function() {
+            return "translate(" + x + "," + y + ")";
+        }).attr('startX', x).attr('startY', y).attr('endX', x + width).attr('endY', y + height);
+        var res = g.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width)
+            .attr("height", height)
+            .attr('stroke', color);
+
+        res += g.append("text").text(text).attr('x', width / 2).attr('y', height / 2).attr('text-anchor', 'middle').attr('dy', '.1em').call(this.wrap, width);
+    }
+
+    //draw rounded rectangle
+    drawRoundRect(id, x, y, width, height, text, color, rx) {
+        if (rx == '' || rx == undefined) {
+            rx = 3;
+        }
+        var g = this.state.svg.append('g').attr('id', 'item' + id).attr('class', 'g_wrapper').attr('transform', function() {
+            return "translate(" + x + "," + y + ")";
+        }).attr('startX', x).attr('startY', y).attr('endX', x + width).attr('endY', y + height);
+        var res = g.append("rect")
+            .attr('ry', rx)
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width)
+            .attr("height", height)
+            .attr('stroke', color);
+        res += g.append("text").text(text).attr('x', width / 2).attr('y', height / 2).attr('text-anchor', 'middle').attr('dy', '.1em').call(this.wrap, width);
+
+    }
+
+    //draw round square and rotate
+    drawRhombus(id, x, y, width, color, rx, text) {
+        var g = this.state.svg.append('g').attr('id', 'item' + id).attr('class', 'g_wrapper').attr('transform', function() {
+            return "translate(" + x + "," + y + ")";
+        }).attr('startX', x).attr('startY', y).attr('endX', x + 2 * this.state.width).attr('endY', y + this.state.width);
+        var res = g.append("polygon")
+            .attr('points', function() {
+                return 0 + ',' + this.state.defElHeight / 2 + ' ' + this.state.width + ',' + (-this.state.width + this.state.defElHeight / 2) + ' ' + (2 * this.state.width) + ',' + this.state.defElHeight / 2 + ' ' + this.state.width + ',' + (this.state.width + this.state.defElHeight / 2)
+            })
+            .attr('stroke', color)
+            .attr('fill', 'white');
+
+        res += g.append("text").text(text).attr('x', this.state.width).attr('y', this.state.defElHeight / 2).attr('text-anchor', 'middle').attr('dy', '.1em').call(this.wrap, this.state.width);
+        return res;
+    }
+
+    drawOrSplitOperator(id, x, y, color, rx) {
+        var g = this.state.svg.append('g').attr('id', 'item' + id).attr('class', 'g_wrapper').attr('transform', function() {
+            return "translate(" + x + "," + y + ")";
+        }).attr('startX', x).attr('startY', y).attr('endX', x + rx).attr('endY', y + rx);
+        g.append("circle")
+            .attr('r', rx)
+            .attr("cx", rx)
+            .attr("cy", rx)
+            .attr('stroke', color);
+        g.append('line')
+            .attr('x1', 0)
+            .attr('y1', rx)
+            .attr('x2', (2 * rx))
+            .attr('y2', (rx))
+            .attr('stroke', this.state.defColor)
+            .attr('stroke-width', '3px');
+
+        g.append('line')
+            .attr('x1', rx)
+            .attr('y1', 0)
+            .attr('x2', rx)
+            .attr('y2', 2 * rx)
+            .attr('stroke', this.state.defColor)
+            .attr('stroke-width', '3px');
+    }
+
+    drawJunctionOperator(id, x, y, color, rx) {
+        var g = this.state.svg.append('g').attr('id', 'item' + id).attr('class', 'g_wrapper').attr('transform', function() {
+            return "translate(" + x + "," + y + ")";
+        }).attr('startX', x).attr('startY', y).attr('endX', x + 2 * rx).attr('endY', y + rx);
+        g.append("circle")
+            .attr('r', rx)
+            .attr("cx", rx)
+            .attr("cy", rx)
+            .attr('stroke', color);
+        g.append('line')
+            .attr('x1', 5)
+            .attr('y1', 5)
+            .attr('x2', (2 * rx - 5))
+            .attr('y2', (2 * rx - 5))
+            .attr('stroke', this.state.defColor)
+            .attr('stroke-width', '3px');
+
+        g.append('line')
+            .attr('x1', 5)
+            .attr('y1', (2 * rx - 5))
+            .attr('x2', (2 * rx - 5))
+            .attr('y2', 5)
+            .attr('stroke', this.state.defColor)
+            .attr('stroke-width', '3px');
+    }
+
+    drawConnectorOperator(id, x, y, color, rx, text) {
+        var g = this.state.svg.append('g').attr('id', 'item' + id).attr('class', 'g_wrapper').attr('transform', function() {
+            return "translate(" + x + "," + y + ")";
+        }).attr('startX', x).attr('startY', y).attr('endX', x + 2 * rx).attr('endY', y + rx);
+        g.append("circle")
+            .attr('r', rx)
+            .attr("cx", rx)
+            .attr("cy", rx / 2)
+            .attr('stroke', color);
+        g.append("text").text(text).attr('x', rx).attr('y', rx / 2).attr('text-anchor', 'middle').attr('dy', '.1em').call(this.wrap, 2 * rx);
+    }
+
+    //Default left-right arrow
+    drawArrow(x, y, nX, nY, nodeType) {
+        var qVH = 3;
+        var ahwidth = 5;
+        switch (nodeType) {
+            case 'start':
+                x += this.state.this.state.defElWidth;
+                y += this.state.defElHeight / 2;
+                break;
+            case 'finish':
+                x += this.state.defElWidth;
+                y += this.state.defElHeight / 2;
+                break;
+            case 'decision':
+                x += 2 * this.state.rhombusRadius;
+                y += this.state.defElHeight / 2;
+                break;
+            case 'process-simple':
+                x += this.state.defElWidth;
+                y += this.state.defElHeight / 2;
+                break;
+            case 'junction':
+                x += 2 * this.state.junctionOperatorRadius;
+                y += this.state.defElHeight / 2;
+                break;
+            case 'or-split':
+                x += 2 * this.state.junctionOperatorRadius;
+                y += this.state.defElHeight / 2;
+                break;
+        }
+
+        return "M" + x + "," + y +
+            "h" + (nX - x - ahwidth) +
+            "v" + (-qVH) +
+            "L" + (nX) + ',' + y +
+            "L" + (nX - ahwidth) + ',' + (parseInt(y) + qVH) +
+            "v" + (-qVH);
+    }
+
+    /*
+    |-------|
+    |       |
+    V       |
+    */
+    drawArrow4(startX, startY, endX, endY) {
+        startX += this.state.defElWidth / 2;
+        return "M" + startX + "," + startY +
+            "v" + (-35) +
+            "h" + -(startX - endX - 20) +
+            "v" + 25 +
+            "h" + 5 +
+            "L" + (endX + 20) + ',' + (endY) +
+            "L" + (endX + 15) + ',' + (endY - 10) +
+            "h" + (5);
+    }
+    //Arrow - Decision to Vertical Element
+    drawArrow2(startX, startY, endX, endY, type) {
+
+        var qVH = 3;
+        startX += 50;
+        if (type == 'decision') {
+            startY += 75;
+            return "M" + startX + "," + startY +
+                "v" + (endY - startY - this.state.defElHeight / 2) +
+                "h" + 5 +
+                "L" + (startX) + ',' + (endY - this.state.defElHeight / 2 + 5) +
+                "L" + (startX - 5) + ',' + (endY - this.state.defElHeight / 2) +
+                "h" + (5);
+        } else {
+            startY += 50;
+            return "M" + startX + "," + startY +
+                "v" + (endY - startY - this.state.defElHeight / 2 - 20) +
+                "h" + 5 +
+                "L" + (startX) + ',' + (endY - this.state.defElHeight / 2 + 5 - 20) +
+                "L" + (startX - 5) + ',' + (endY - this.state.defElHeight / 2 - 20) +
+                "h" + (5);
+        }
+
+    }
+
+    //Arrow - bottom to top left
+    drawArrow3(startX, startY, endX, endY) {
+        var qVH = 3;
+        endX += this.state.junctionOperatorRadius;
+        startY += this.state.defElHeight / 2;
+        endY += 2 * this.state.junctionOperatorRadius + 8;
+        return "M" + startX + "," + startY +
+            "h" + (endX - startX) +
+            "v" + (endY - startY) +
+            "h" + (-5) +
+            "L" + (endX) + ',' + (endY - 5) +
+            "L" + (endX + 5) + ',' + (endY) +
+            "h" + (-5);
+    }
+
+    wrap(text, width) {
+        text.each(function() {
+            var text = d3.select(this),
+                words = text.text().split(/\s+/).reverse(),
+                word,
+                line = [],
+                lineNumber = 0,
+                lineHeight = 1.1, // ems
+                y = text.attr("y"),
+                x = text.attr('x'),
+                dy = parseFloat(text.attr("dy")),
+                tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+            while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                }
+            }
+        });
+    }
+
+    parseJson(jsondata) {
+        this.state.totalItemsCnt = Object.keys(jsondata).length;
+        var connectorsArray = [];
+        for (var index in jsondata) {
+            connectorsArray.push(Object.keys(jsondata[index].connectors).length);
+        }
+        this.state.depth = d3.max(connectorsArray);
+        this.setState({'defElWidth' : this.state.width / (this.state.totalItemsCnt - this.state.depth + 1) - this.state.linkWidth})
+    }
+
+    selectArrow(startX, startY, endX, endY, nodeType) {
+        if (startY == endY) {
+            if (endX < startX) {
+                return this.drawArrow4(startX, startY, endX, endY);
+            } else {
+                return this.drawArrow(startX, startY, endX, endY, nodeType);
+            }
+        }
+
+        if (startX == endX) {
+            return this.drawArrow2(startX, startY, endX, endY, nodeType);
+        }
+
+        if ((endX < startX) && (endY < startY)) {
+            return this.drawArrow3(startX, startY, endX, endY);
+        }
+    }
+    drawElement(startX, startY, data, step) {
+      var nextX, nextY;
+        this.state.drawedItemsArray.push(step);
+        // step++;        
+        switch (data.type) {
+            case 'start':
+                this.drawRoundRect(data.id, startX, startY, this.state.defElWidth, 40, data.title, this.state.defColor);
+                break;
+            case 'finish':
+                this.drawRoundRect(data.id, startX, startY, this.state.defElWidth, 40, data.title, this.state.priColor);
+                break;
+            case 'process-simple':
+                this.drawRect(data.id, startX, startY, this.state.defElWidth, 40, this.state.defColor, data.title);
+                break;
+            case 'decision':
+                this.drawRhombus(data.id, startX, startY, this.state.rhombusRadius, this.state.defColor, 5, data.title);
+                break;
+            case 'connector-start':
+                this.drawConnectorOperator(data.id, startX, startY, this.state.defColor, 40, data.title)
+                break;
+            case 'connector-end':
+                this.drawConnectorOperator(data.id, startX, startY, this.state.priColor, 40, data.title)
+                break;
+            case 'or-split':
+                this.drawOrSplitOperator(data.id, startX, startY, this.state.defColor, 20);
+                break;
+            case 'junction':
+                this.drawJunctionOperator(data.id, startX, startY, this.state.defColor, 20);
+                break;
+            default:
+                break;
+        }
+
+        if (Object.keys(data.connectors).length == 1) {
+            var tmpConnector = data.connectors[1];
+            var nextStep = tmpConnector.linkTo;
+            var nextItemData = this.state.itemsData[nextStep];
+            if (data.stream == nextItemData.stream) {
+                nextX = startX + this.state.defElWidth + this.state.linkWidth + this.state.padding;
+                nextY = startY;
+            } else {
+                nextX = startX;
+                nextY = this.state.defStreamHeight * (nextItemData.stream - 1) + this.state.paddingY;
+            }
+
+            if (this.state.drawedItemsArray.indexOf(nextStep) == -1) {
+                this.drawElement(nextX, nextY, nextItemData, nextStep);
+            }
+        }
+
+        if (Object.keys(data.connectors).length == 2) {
+            //1st node
+            var tmpConnector = data.connectors[1];
+            var nextStep = tmpConnector.linkTo;
+            var nextItemData = this.state.itemsData[nextStep];
+            if (data.stream == nextItemData.stream) {
+                nextX = startX + this.state.defElWidth + this.state.linkWidth + this.state.padding;
+                nextY = startY;
+            } else {
+                nextX = startX;
+                nextY = this.state.defStreamHeight * (nextItemData.stream - 1) + this.state.paddingY;
+            }
+
+            if (this.state.drawedItemsArray.indexOf(nextStep) == -1) {
+                this.drawElement(nextX, nextY, nextItemData, nextStep);
+            }
+
+            //2nd node
+            tmpConnector = data.connectors[2];
+            nextStep = tmpConnector.linkTo;
+            nextItemData = this.state.itemsData[nextStep];
+            if (data.stream == nextItemData.stream) {
+                nextX = startX;
+                nextY = startY + 120;
+            } else {
+                nextX = startX;
+                nextY = this.state.defStreamHeight * (nextItemData.stream - 1) + this.state.paddingY;
+            }
+
+            if (this.state.drawedItemsArray.indexOf(nextStep) == -1) {
+                this.drawElement(nextX, nextY, nextItemData, nextStep);
+            }
+        }
+    }
+
+    drawLinks(itemsData) {
+        for (var index in this.state.itemsData) {
+            for (var connector in this.state.itemsData[index].connectors) {
+                var fromId = this.state.itemsData[index].id;
+                var startX = d3.select('#item' + fromId).attr('startX');
+                var startY = d3.select('#item' + fromId).attr('startY');
+                var toId = this.state.itemsData[index].connectors[connector].linkTo;
+                var endX = d3.select('#item' + toId).attr('startX');
+                var endY = d3.select('#item' + toId).attr('startY');
+                var nodeType = this.state.itemsData[index].type;
+                // endY = d3.select('#item'+toId).attr('startY');
+                this.state.svg.append('path').attr("d", this.selectArrow(parseInt(startX), parseInt(startY), parseInt(endX), parseInt(endY), nodeType)).attr("fill", "none");
+            }
+        }
+    }
+
+    drawStreamLayout(data) {
+        var streamsData = data.streams;
+        var streamsCnt = Object.keys(streamsData).length;
+
+        this.state.defStreamHeight = this.state.height / streamsCnt;
+        var streamsArr = [];
+        for (var stream in streamsData) {
+            streamsArr.push({
+                'id': streamsData[stream].id,
+                'order': streamsData[stream].order,
+                'title': streamsData[stream].title
+            })
+        };
+
+        var g_stream_wrapper = this.state.svg.selectAll(".stream")
+            .data(streamsArr)
+            .enter().append('g').attr('class', 'stream_wrapper');
+
+        g_stream_wrapper
+            .append("rect")
+            .attr("class", "stream")
+            .attr("x", 0)
+            .attr("y", function(d, i) {
+                return this.state.height / streamsCnt * i
+            }).attr('rx', 6).attr('width', this.state.width).attr('height', function(d, i) {
+                return this.state.height / streamsCnt;
+            }).attr('fill', 'none').attr('stroke', '#ddd').attr('stroke-width', '2px');
+
+        g_stream_wrapper.append('text')
+            .attr('x', function(d, i) {
+                return -this.state.height / streamsCnt * i
+            })
+            .attr('y', 0)
+            .style("text-anchor", "end")
+            .attr("dx", "-3.5em")
+            .attr("dy", "-.55em")
+            .attr("transform", "rotate(-90)").text(function(d) {
+                return d.title
+            });
+    }
+    render() {
+        return ( <
+            div ref = "workflow" > < /div>
+        )
+    }
 }
 
 export default WorkflowChart;
